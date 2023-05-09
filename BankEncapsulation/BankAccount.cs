0401361d57dfe0d8a3de63f46d992c5cedf5bc78
AlissaStegall <alissaistegall@gmail.com>
2023-05-09 15:15:16 -0700

    completed encapsulation exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+        public BankAccount()
+        {
+        }
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+    }
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountDeposited = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountDeposited);
+
+            Console.WriteLine($"Your balance is now {account.GetBalance()}");
         }
     }
 }
